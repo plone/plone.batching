a13de315f9b692d87d5f9355623b8e4247d1afab
Tom Gross <tom@toms-projekte.de>
2011-10-08 20:37:59 +0200

    massive restructure of code

--- plone/batching/batch.py
+++ plone/batching/batch.py
@@ -2,49 +2,35 @@ from ZTUtils.Batch import Batch as ZTUBatch
 from ZTUtils import make_query
 from ExtensionClass import Base
 
-# These have to be duplicated from ZTUtils.Batch to use the correct Batch
-# class, otherwise orphans will come out wrong in the 'show x next messages'.
+from plone.batching.interfaces import IBatch
+from zope.interface import implements
+
+from plone.batching.utils import (
+    opt, calculate_leapback, calculate_leapforward,
+    calculate_pagenumber, calculate_pagerange, calculate_quantum_leap_gap)
 
 
-class LazyPrevBatch(Base):
+class Batch(object):
+    """ A sequence batch implementation
+    """
 
-    def __of__(self, parent):
-        return Batch(parent._sequence, parent._size,
-                     parent.first - parent._size + parent.overlap, 0,
-                     parent.orphan, parent.overlap)
+    implements(IBatch)
 
+    b_start_str = 'b_start'
+    quantumleap = 0
+    leapback = leapforward = 0
 
-class LazyNextBatch(Base):
+    def __init__(self, items, pagesize=20, pagenumber=1, navlistsize=5, orphan=0, overlap=0):
+        """
+        Encapsulate sequence in batches of pagesize.
+        """
+        self.items = items
+        self.pagesize = pagesize
+        self.pagenumber = pagenumber
+        self.navlistsize = navlistsize
 
-    def __of__(self, parent):
-        if parent.end >= (parent.last + parent.size):
-            return None
-        return Batch(parent._sequence, parent._size,
-                     parent.end - parent.overlap, 0,
-                     parent.orphan, parent.overlap)
-
-
-class LazySequenceLength(Base):
-
-    def __of__(self, parent):
-        _sequence = parent._sequence
-        l = getattr(_sequence, 'actual_result_count', len(_sequence))
-        parent.sequence_length = l
-        return l
-
-
-class Batch(ZTUBatch):
-    """Create a sequence batch"""
-    __allow_access_to_unprotected_subobjects__ = 1
-
-    previous = LazyPrevBatch()
-    next = LazyNextBatch()
-    sequence_length = LazySequenceLength()
-
-    size = first= start = end = orphan = overlap = navlist = None
-    numpages = pagenumber = pagerange = pagerangeend = pagerangestart = pagenumber = quantumleap = None
-
-    def __init__(self, sequence, size, start=0, end=0, orphan=0, overlap=0, pagerange=7, quantumleap=0, b_start_str='b_start'):
+    def fromStartEnd(cls, sequence, size, start=0, end=0, orphan=0, overlap=0,
+                 pagerange=7, quantumleap=0, b_start_str='b_start'):
         """ Encapsulate sequence in batches of size
         sequence    - the data to batch.
         size        - the number of items in each batch. This will be computed if left out.
@@ -56,173 +42,141 @@ class Batch(ZTUBatch):
         quantumleap - 0 or 1 to indicate if bigger increments should be used in the navigation list for big results.
         b_start_str - the request variable used for start, default 'b_start'
         """
-        start = start + 1
-        self._sequence = sequence
-        self._size = size
-        sequence_length = self.sequence_length
 
-        start, end, sz = opt(start, end, size, orphan, sequence_length)
+        start, end, sz = opt(start, end, size, orphan, len(sequence))
+        pagenumber = calculate_pagenumber(start, size, overlap)
 
-        self.size = sz
-        self.start = start
-        self.end = end
-        self.orphan = orphan
-        self.overlap = overlap
-        self.first = max(start - 1, 0)
-        self.length = self.end - self.first
+#        self.b_start_str = b_start_str
+#
+#        # QuantumLeap - faster navigation for big result sets
+#        self.quantumleap = quantumleap
+#        self.leapback = self.leapforward = []
+#        if self.quantumleap:
+#            self.leapback = calculate_leapback(self.pagenumber, self.numpages, self.pagerange)
+#            self.leapforward = calculate_leapforward(self.pagenumber, self.numpages, self.pagerange)
 
-        self.b_start_str = b_start_str
+        return cls(sequence, size, pagenumber, pagerange, orphan, overlap)
+    fromStartEnd = classmethod(fromStartEnd)
 
-        self.last = sequence_length - size
+    def fromRange(cls, length, pagesize=20, pagenumber=1, navlistsize=5):
+        """
+        Create a batch from a range of a given length
+        """
+        items = range(length)
+        return cls(items, pagesize, pagenumber, navlistsize)
+    fromRange = classmethod(fromRange)
+            
+    def __len__(self):
+        return len(self.items)
 
-        # Set up next and previous
-        if self.first == 0:
-            self.previous = None
+    @property
+    def firstpage(self):
+        return 1
 
-        # Set up the total number of pages
-        self.numpages = calculate_pagenumber(self.sequence_length - self.orphan, self.size, self.overlap)
+    @property
+    def size(self):
+        return len(self)
+        
+    def __iter__(self):
+        """
+        Iterate over a single batch
+        """
+        start = (self.pagenumber-1) * self.pagesize
+        end = start + self.pagesize
+        return self.items[start:end].__iter__()
 
-        # Set up the current page number
-        self.pagenumber = calculate_pagenumber(self.start, self.size, self.overlap)
-
-        # Set up pagerange for the navigation quick links
-        self.pagerange, self.pagerangestart, self.pagerangeend = calculate_pagerange(self.pagenumber, self.numpages, pagerange)
-
-        # Set up the lists for the navigation: 4 5 [6] 7 8
-        #  navlist is the complete list, including pagenumber
-        #  prevlist is the 4 5 in the example above
-        #  nextlist is 7 8 in the example above
-        self.navlist = self.prevlist = self.nextlist = []
-        if self.pagerange and self.numpages >= 1:
-            self.navlist = range(self.pagerangestart, self.pagerangeend)
-            self.prevlist = range(self.pagerangestart, self.pagenumber)
-            self.nextlist = range(self.pagenumber + 1, self.pagerangeend)
-
-        # QuantumLeap - faster navigation for big result sets
-        self.quantumleap = quantumleap
-        self.leapback = self.leapforward = []
-        if self.quantumleap:
-            self.leapback = calculate_leapback(self.pagenumber, self.numpages, self.pagerange)
-            self.leapforward = calculate_leapforward(self.pagenumber, self.numpages, self.pagerange)
-
-    def pageurl(self, formvariables, pagenumber=-1):
-        """ Makes the url for a given page """
-        if pagenumber == -1:
-            pagenumber = self.pagenumber
-        b_start = pagenumber * (self.size - self.overlap) - self.size
-        return make_query(formvariables, {self.b_start_str: b_start})
-
-    def navurls(self, formvariables, navlist=[]):
-        """ Returns the page number and url for the navigation quick links """
-        if not navlist:
-            navlist = self.navlist
-        return map(lambda x, formvariables = formvariables: (x, self.pageurl(formvariables, x)), navlist)
-
-    def prevurls(self, formvariables):
-        """ Helper method to get prev navigation list from templates """
-        return self.navurls(formvariables, self.prevlist)
-
-    def nexturls(self, formvariables):
-        """ Helper method to get next navigation list from templates """
-        return self.navurls(formvariables, self.nextlist)
-
-    def __getitem__(self, index):
-        actual = getattr(self._sequence, 'actual_result_count', None)
-        if actual is not None and actual != len(self._sequence):
-            # optmized batch that contains only the wanted items in the sequence
-            return self._sequence[index]
-        if index < 0:
-            if index + self.end < self.first:
-                raise IndexError(index)
-            return self._sequence[index + self.end]
-        if index >= self.length:
-            raise IndexError(index)
-        return self._sequence[index + self.first]
-
-
-# Calculate start, end, batchsize
-# This is copied from ZTUtils.Batch.py because orphans were not correct there.
-# 04/16/04 modified by Danny Bloemendaal (_ender_). Removed try/except structs because
-# in some situations they cause some unexpected problems.
-# Also fixed some problems with the orphan stuff. Seems to work now.
-def opt(start, end, size, orphan, sequence_length):
-    length = sequence_length
-    if size < 1:
-        if start > 0 and end > 0 and end >= start:
-            size = end + 1 - start
-        else:
-            size = 25
-    if start > 0:
-        if start>length:
-            start = length
-        if end > 0:
-            if end < start:
-                end = start
-        else:
-            end = start + size - 1
-            if (end+orphan)>=length:
-                end = length
-    elif end > 0:
-        if (end)>length:
-            end = length
-        start = end + 1 - size
-        if start - 1 < orphan:
+    @property
+    def navlist(self):
+        start = self.pagenumber - (self.navlistsize / 2)
+        if start < 1:
             start = 1
-    else:
-        start = 1
-        end = start + size - 1
-        if (end+orphan)>=length:
-            end = length
-    return start, end, size
+        end = start + self.navlistsize - 1
+        if end > self.lastpage:
+            end = self.lastpage
+
+        return range(start, end+1)
+
+    @property
+    def items_on_page(self):
+        if self.islastpage:
+            remainder = self.size % self.pagesize
+            if remainder == 0:
+                return self.pagesize
+            else:
+                return remainder
+        else:
+            return self.pagesize
+
+    @property
+    def items_not_on_page(self):
+        start = (self.pagenumber-1) * self.pagesize
+        end = start + self.pagesize
+        return self.items[:start] + self.items[end:]
+
+    @property
+    def next_item_count(self):
+        """
+        Number of elements in next batch
+        """
+        nextitems = self.size - (self.pagenumber * self.pagesize)
+        if nextitems > self.pagesize:
+            return self.pagesize
+        return nextitems
+
+    @property
+    def islastpage(self):
+        return self.lastpage == self.pagenumber
+
+    @property
+    def multiple_pages(self):
+        return bool(self.size / self.pagesize)
+
+    @property
+    def has_next(self):
+        return (self.pagenumber * self.pagesize) < self.size
+
+    @property
+    def show_link_to_first(self):
+        return 1 not in self.navlist
+
+    @property
+    def show_link_to_last(self):
+        return self.lastpage not in self.navlist
+
+    @property
+    def before_last_page_not_in_navlist(self):
+        return (self.lastpage - 1) not in self.navlist
+
+    @property
+    def has_previous(self):
+        return self.pagenumber > 1
+
+    @property
+    def previous_pages(self):
+        return self.navlist[:self.navlist.index(self.pagenumber)]
+    
+    @property
+    def next_pages(self):
+        return self.navlist[self.navlist.index(self.pagenumber)+1:]
+
+    @property
+    def previouspage(self):
+        return self.pagenumber - 1
+
+    @property
+    def nextpage(self):
+        return self.pagenumber + 1
+
+    @property
+    def lastpage(self):
+        pages = self.size / self.pagesize
+        if self.size % self.pagesize:
+            pages += 1
+        return pages
+
+    @property
+    def second_page_not_in_navlist(self):
+        return 2 not in self.navlist
 
 
-def calculate_pagenumber(elementnumber, batchsize, overlap=0):
-    """ Calculate the pagenumber for the navigation """
-    # To find first element in a page,
-    # elementnumber = pagenumber * (size - overlap) - size (- orphan?)
-    try:
-        pagenumber, remainder = divmod(elementnumber, batchsize - overlap)
-    except ZeroDivisionError:
-        pagenumber, remainder = divmod(elementnumber, 1)
-    if remainder > overlap:
-        pagenumber = pagenumber + 1
-    pagenumber = max(pagenumber, 1)
-    return pagenumber
 
-
-def calculate_pagerange(pagenumber, numpages, pagerange):
-    """ Calculate the pagerange for the navigation quicklinks """
-    # Pagerange is the number of pages linked to in the navigation, odd number
-    pagerange = max(0, pagerange + pagerange % 2 - 1)
-    # Making sure the list will not start with negative values
-    pagerangestart = max(1, pagenumber - (pagerange - 1) / 2)
-    # Making sure the list does not expand beyond the last page
-    pagerangeend = min(pagenumber + (pagerange - 1) / 2, numpages) + 1
-    return pagerange, pagerangestart, pagerangeend
-
-
-def calculate_quantum_leap_gap(numpages, pagerange):
-    """ Find the QuantumLeap gap. Current width of list is 6 clicks (30/5) """
-    return int(max(1, round(float(numpages - pagerange)/30))*5)
-
-
-def calculate_leapback(pagenumber, numpages, pagerange):
-    """ Check the distance between start and 0 and add links as necessary """
-    leapback = []
-    quantum_leap_gap = calculate_quantum_leap_gap(numpages, pagerange)
-    num_back_leaps = max(0, min(3, int(round(float(pagenumber - pagerange)/quantum_leap_gap) - 0.3)))
-    if num_back_leaps:
-        pagerange, pagerangestart, pagerangeend = calculate_pagerange(pagenumber, numpages, pagerange)
-        leapback = range(pagerangestart - num_back_leaps * quantum_leap_gap, pagerangestart, quantum_leap_gap)
-    return leapback
-
-
-def calculate_leapforward(pagenumber, numpages, pagerange):
-    """ Check the distance between end and length and add links as necessary """
-    leapforward = []
-    quantum_leap_gap = calculate_quantum_leap_gap(numpages, pagerange)
-    num_forward_leaps = max(0, min(3, int(round(float(numpages - pagenumber - pagerange)/quantum_leap_gap) - 0.3)))
-    if num_forward_leaps:
-        pagerange, pagerangestart, pagerangeend = calculate_pagerange(pagenumber, numpages, pagerange)
-        leapforward = range(pagerangeend-1 + quantum_leap_gap, pagerangeend-1 + (num_forward_leaps+1) * quantum_leap_gap, quantum_leap_gap)
-    return leapforward
--- plone/batching/browser.py
+++ plone/batching/browser.py
@@ -1,5 +1,33 @@
 from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from ZTUtils import  make_query
 
-class BatchMacros(BrowserView):
-    """ View class for browser navigation """
+BatchTemplate = ViewPageTemplateFile("batchnavigation.pt")
+
+class BatchMacrosView(BrowserView):
+
+    @property
+    def macros(self):
+        return self.template.macros
+
+class BatchView(BrowserView):
+    """ View class for browser navigation  (classic) """
+
+    template = BatchTemplate
+
+    def __call__(self, batch):
+        self.batch = batch
+        return self.template()
+    
+    def make_link(self, pagenumber):
+        # XXX implement batchformkeys
+        batchformkeys = None
+        form = self.request.form
+        if batchformkeys:
+            batchlinkparams = dict([(key, form[key]) for key in batchformkeys if key in form])
+        else:
+            batchlinkparams = form.copy()
+
+        start = max(pagenumber-1, 0)  * self.batch.pagesize
+        return '%s?%s' % (self.request.ACTUAL_URL , make_query( batchlinkparams, {self.batch.b_start_str:start} ))
 
