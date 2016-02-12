4c295bb32357f88a71157969013abf25e28ef3d7
Maurits van Rees <maurits@vanrees.org>
2016-02-12 15:22:04 +0100

    Set classifiers to Plone 5.0, Python 2.7.
    For Plone 4.3, use fresh branch 1.0.x.

--- setup.py
+++ setup.py
@@ -15,12 +15,10 @@ setup(
     ]),
     classifiers=[
         "Framework :: Plone",
-        "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
-        'Framework :: Zope2',
+        "Framework :: Zope2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        'Programming Language :: Python :: 2.6',
     ],
     keywords='Plone',
     author='Plone Foundation',
