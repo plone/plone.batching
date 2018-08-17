13a19387a835c354e9ff44c4f36c6abeac798c53
Jens W. Klein <jk@kleinundpartner.at>
2018-08-17 10:42:31 +0200

    update troove classifier

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
     ],
     keywords='Plone',
     author='Plone Foundation',
