34d04f925c9edb6d916011942d88213b9953d409
Gil Forcada Codinachs <gil.gnome@gmail.com>
2018-11-10 22:19:34 +0100

    Update trove classifiers
    [ci-skip]

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     keywords='Plone',
     author='Plone Foundation',
